427f1eb20bb0a52a40b63e8b7ace7458382b14fe
Ivannnnnn886 <ivanmaslov2007@gmail.com>
2025-02-02 15:29:36 +0100

    django edit static root

--- main/context_processors.py
+++ main/context_processors.py
@@ -4,12 +4,23 @@ def contact_info(request):
     data=ContactInfo.objects.first()
 
 
-    return {
-        'city': data.city_address,
-        'street': data.street_address,
-        'phone_number': data.phone_number,
-        'email': data.email,
-        'opening_hours': data.opening_hours,
+    if data:
+        return {
+           'city': data.city_address,
+           'street': data.street_address,
+           'phone_number': data.phone_number,
+           'email': data.email,
+           'opening_hours': data.opening_hours,
 
-        'instagram': data.instagram_link,
+           'instagram': data.instagram_link,
+       }
+
+    return {
+        'city': '',
+        'street': '',
+        'phone_number': '',
+        'email': '',
+        'opening_hours': '',
+
+        'instagram': '',
     }
